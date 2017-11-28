762a3af188f237f70c20a96775bfe73192ad52c4
Maegner <franciscommaguiar@gmail.com>
2017-11-28 18:54:45 +0000

    querie refactoring

--- parte3/queriesChico.sql
+++ parte3/queriesChico.sql
@@ -1,39 +1,31 @@
 --a
-WITH Produtor_primario AS (
-	SELECT forn_primario as nif, ean
-	FROM Produto
-), Produtores_por_produto as (
+WITH Produtores_por_produto as (
 
-	SELECT * FROM Produtor_primario 
+	SELECT forn_primario as nif,ean FROM Produto 
 	UNION 
 	SELECT * FROM Fornecedor_secundario
 
-), Categoria_por_produto as (
-	SELECT ean, categoria
-	FROM Produto
-
 ),Categoria_por_fornecedor as (
-	SELECT * 
-	FROM Categoria_por_produto, Produtores_por_produto 
-	WHERE Categoria_por_produto.ean = Produtores_por_produto.ean
+	SELECT nif,categoria 
+	FROM Produto, Produtores_por_produto 
+	WHERE Produto.ean = Produtores_por_produto.ean
 
 ), n_Categorias_por_fornecedor as (
 	SELECT nif, COUNT(categoria) as n_categorias
 	FROM Categoria_por_fornecedor
 	GROUP BY nif
 
-), max_Categorias as (
-	SELECT MAX(n_categorias) as max_cat
-	FROM n_Categorias_por_fornecedor
-
-), nif_fornecedor_com_max_cat as (
-	SELECT nif
-	FROM max_Categorias, n_Categorias_por_fornecedor
-	WHERE max_Categorias.max_cat = n_Categorias_por_fornecedor.n_categorias
 )
 SELECT nome
-FROM nif_fornecedor_com_max_cat,Fornecedor
-WHERE nif_fornecedor_com_max_cat.nif = Fornecedor.nif;
+FROM Fornecedor
+WHERE Fornecedor.nif IN (
+	SELECT nif
+	FROM n_Categorias_por_fornecedor
+	WHERE n_Categorias_por_fornecedor.n_categorias IN(
+		SELECT MAX(n_categorias) as max_cat
+		FROM n_Categorias_por_fornecedor
+	)
+);
 
 --b
 WITH Produtor_primario AS (
@@ -41,37 +33,27 @@ WITH Produtor_primario AS (
 	FROM Produto
 ), Produtores_por_produto as (
 
-	SELECT * FROM Produtor_primario 
+	SELECT nif,ean FROM Produtor_primario 
 	UNION 
 	SELECT * FROM Fornecedor_secundario
 
-), Categoria_por_produto as (
-	SELECT ean, categoria
-	FROM Produto
-
-),Categoria_por_fornecedor as (
+),Categorias_por_fornecedor as (
 	SELECT * 
-	FROM Categoria_por_produto, Produtores_por_produto 
-	WHERE Categoria_por_produto.ean = Produtores_por_produto.ean
-
-),Categorias_simples_por_fornecedor as (
-	SELECT * 
-	FROM Categoria_por_fornecedor,Categoria_Simples
-	WHERE Categoria_por_fornecedor.categoria = Categoria_Simples.nome
+	FROM Produto, Produtores_por_produto 
+	WHERE Produto.ean = Produtores_por_produto.ean AND
+	Produto.categoria IN (SELECT nome FROM Categoria_Simples)
 
 ),n_Categorias_simples_por_fornecedor as(
 	SELECT nif, COUNT(distinct categoria) as n_categorias_simples
-	FROM Categorias_simples_por_fornecedor
+	FROM Categorias_por_fornecedor
 	GROUP BY nif
-
-),n_total_Categorias_simples as(
-	SELECT COUNT(distinct nome) as n_total
-	FROM Categoria_Simples
-),nif_de_todos_que_forn as(
-	SELECT nif
-	FROM n_total_Categorias_simples,n_Categorias_simples_por_fornecedor
-	WHERE n_total_Categorias_simples.n_total = n_Categorias_simples_por_fornecedor.n_categorias_simples
 )
 SELECT Fornecedor.nif, nome
-FROM nif_de_todos_que_forn, Fornecedor
-WHERE nif_de_todos_que_forn.nif = Fornecedor.nif;
+FROM Fornecedor
+WHERE Fornecedor.nif IN (
+	SELECT nif
+	FROM n_Categorias_simples_por_fornecedor
+	WHERE n_Categorias_simples_por_fornecedor.n_categorias_simples IN (
+		SELECT COUNT(distinct nome) as n_categorias_simples
+		FROM Categoria_Simples)
+);
