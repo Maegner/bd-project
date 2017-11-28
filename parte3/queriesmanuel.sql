6f0b71efc04c18feaaf93305afdec747bcba385b
Manuel Sousa <manuelsousamvs@gmail.com>
2017-11-28 00:51:08 +0000

    queries added

---
+++ parte3/queriesmanuel.sql
@@ -0,0 +1,13 @@
+c)
+
+SELECT ean from Produto where ean not in 
+(SELECT ean from Reposicao);
+
+d)
+
+SELECT ean from Produto where ean in
+(SELECT ean from Fornecedor_secundario group by ean having count(ean)>10);
+
+e)
+
+SELECT ean from Reposicao group by ean having count(distinct operador) = count(operador);
