75c7074bde73b1612a23e0e645c7d61ba77263c6
Goncalo Marques <goncalo.cool@gmail.com>
2017-11-29 20:39:59 +0000

    Correção

--- parte3/web/produto.php
+++ parte3/web/produto.php
@@ -62,10 +62,7 @@
     $data = $_REQUEST['DataProduto'];
     $remover = $_REQUEST['RemoverProduto'];
 
-    if(!isRealDate($data)){
-        echo("<p>ERROR: date inserted is not valid please insert in format YYYY-MM-DD</p>");
-    }
-
+    
     $fornecedores_sec = array();
     $int = intval($secundarios);
     for($i = 0; $i < $int; ++$i) {
@@ -74,31 +71,10 @@
         echo("<script>console.log($string)</script>");
         array_push($fornecedores_sec, $string);
     }
-
-    try
-    {
-        $host = "db.ist.utl.pt";
-        $user ="istxxxxx";
-        $password = "xxxxxxx";
-        $dbname = $user;
-        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        if ($remover != "on") {
-            $sql = "INSERT INTO Produto(ean, categoria, forn_primario, design, data)
-                    VALUES ($ean, $categoria, $primario, $designacao, $data);"
-            ;
-        }// Tem de fazer input de um fornecedor secundario tbem RI-RE3​: Todo o EAN​ de produto​ tem de existir na relação fornece_sec
-        else {
-            $sql = "DELETE FROM Produto
-                    WHERE ean = $ean;"
-            ;
-        }
-
-        $db->query($sql);
-        $db->query("commit;");
-        $db = null;
     
+    if(!isRealDate($data)){
+        echo("<p>ERROR: date inserted is not valid please insert in format YYYY-MM-DD</p>");
+    }
     else{
         try
         {
