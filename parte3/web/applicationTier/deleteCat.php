5483f5077b34a766ae68bbb1fc5a19002b7ca078
Maegner <franciscommaguiar@gmail.com>
2017-11-30 19:18:42 +0000

    deletes adicionados ainda nao testados

---
+++ parte3/web/applicationTier/deleteCat.php
@@ -0,0 +1,27 @@
+<html>
+    <body>
+<?php
+
+    $nomeCategoria = $_REQUEST['NomeCategoria'];
+    try
+    {
+        $host = "db.ist.utl.pt";
+        $user ="istxxxxx";
+        $password = "xxxxxxx";
+        $dbname = $user;
+        $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "DELETE FROM Categoria WHERE nome = '$nomeCategoria';";
+
+        $db->query($sql);
+
+        $db = null;
+    }
+    catch (PDOException $e)
+    {
+        echo("<p>ERROR: {$e->getMessage()}</p>");
+    }
+?>
+    </body>
+</html>
