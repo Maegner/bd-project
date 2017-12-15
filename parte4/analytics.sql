ee3affbc5cae1edb6867486532999e8fba5163b8
Maegner <franciscommaguiar@gmail.com>
2017-12-15 20:51:08 +0000

    _final analytics

--- parte4/analytics.sql
+++ parte4/analytics.sql
@@ -1,4 +1,4 @@
-SELECT categoria, SUM(numero_reposicoes)
-FROM info_reposicao,d_produto
+SELECT categoria,ano,mes,SUM(numero_reposicoes)
+FROM info_reposicao NATURAL JOIN d_produto
 WHERE nif_fornecedor_principal = '123455678'
 GROUP BY ROLLUP(categoria, ano, mes);
